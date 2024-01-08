319c7bf5cfd85e42bf7def4ed7a2ed9b2178e744
matheus <matheus_aparecido@discente.ufg.br>
2024-01-08 01:14:46 -0300

    Implementação do dimmer

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,71 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
+// Configurações de rede WiFi
 const char* ssid = "MATHEUS ";
 const char* password = "12213490";
+
+// Configurações MQTT
 const char* mqtt_username = "admin";
 const char* mqtt_password = "1221";
-const char* mqtt_broker_address = "ec2-13-58-196-72.us-east-2.compute.amazonaws.com";
-const char* topic = "esp32/atenuador";
-const int ledPin = 2; // GPIO onde o LED está conectado
+const char* mqtt_broker_address = "35.208.123.29";
+const char* topic = "esp32/pessoa_det";
+const byte ZCP = 13;
+const byte TRIAC_PIN = 12;
+unsigned int dimDuration = 5000;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 unsigned long lastMsg = 0;
-bool ledState = false;
 
-void setup_wifi() {
-  delay(10);
-  Serial.println();
-  Serial.print("Conectando-se a ");
-  Serial.println(ssid);
-
+// Função para conectar ao Wi-Fi
+void connectWiFi() {
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
+
+  Serial.println("\n\n=====================");
+  Serial.println("Conectando no Wi-Fi");
+  Serial.println("=====================\n\n");
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
-  Serial.println("");
-  Serial.println("WiFi conectado");
-  Serial.println("Endereço IP: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("\n\n=====================");
+  Serial.println("Conectado ao Wi-Fi!");
+  Serial.println("=====================\n\n");
 }
 
+// Função de retorno de chamada para mensagens MQTT recebidas
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Mensagem recebida no tópico: ");
-  Serial.println(topic);
+  Serial.println("Pessoa detectada! ");
 
-  int value = payload[0] - '0'; // Converte o payload para um número inteiro
+  int value = payload[0] - '0';
 
   if (value == 1) {
-    ledState = true;
     lastMsg = millis();
-    digitalWrite(ledPin, HIGH);
-    Serial.println("LED ligado");
+    digitalWrite(TRIAC_PIN, HIGH);
+    Serial.println("Lâmpada ligada!\n");
+    
+    // Atualiza o valor de dimDuration
+    dimDuration = 1000;
+    Serial.printf("Intensidade luminosa alterada de 50%% para 100%%");
   }
 }
 
-void reconnect() {
+// Função para reconectar ao servidor MQTT
+void reconnectMQTT() {
   while (!client.connected()) {
-    Serial.print("Conectando ao servidor MQTT...");
-    if (client.connect("ESP32Client", mqtt_username, mqtt_password)) {
-      Serial.println("Conectado!");
+    Serial.println("\n\n=====================");
+    Serial.println("Conectando no MQTT BROKER");
+    Serial.println("=====================\n\n");
+
+    if (client.connect("Atenuador1", mqtt_username, mqtt_password)) {
+      Serial.println("\n\n=====================");
+      Serial.println("Conectado ao MQTT!");
+      Serial.println("=====================\n\n");      
       client.subscribe(topic);
     } else {
       Serial.print("Falha, rc=");
@@ -66,23 +79,22 @@ void reconnect() {
 void setup() {
   Serial.begin(115200);
 
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
+  pinMode(TRIAC_PIN, OUTPUT);
+  digitalWrite(TRIAC_PIN, LOW);
 
-  setup_wifi();
+  connectWiFi();
+
   client.setServer(mqtt_broker_address, 1883);
   client.setCallback(callback);
 }
 
 void loop() {
   if (!client.connected()) {
-    reconnect();
+    reconnectMQTT();
   }
   client.loop();
 
-  if (ledState && millis() - lastMsg > 5000) {
-    ledState = false;
-    digitalWrite(ledPin, LOW);
-    Serial.println("LED desligado após 5 segundos sem receber 1");
+  if (millis() - lastMsg > 5000) {
+    digitalWrite(TRIAC_PIN, LOW);
   }
 }
